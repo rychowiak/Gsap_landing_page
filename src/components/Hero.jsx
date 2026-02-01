96eb7fb1b638de0af622d73f65b4babd342194b8
Ricardo <ricardosepu93@gmail.com>
2026-02-01 16:26:20 -0700

    added hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,39 @@
+const Hero = () => {
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">MOJITO</h1>
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left-leaf"
+          className="left-leaf"
+        />
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right-leaf"
+          className="right-leaf"
+        />
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crisp. Classic.</p>
+              <p className="subtitle">
+                Sip the spirit <br /> of summer
+              </p>
+            </div>
+            <div className="view-cocktails">
+              <p className="subtitle">
+                every cocktail on our menu is a blend of premuim ingredients,
+                cretive flair, and timeless recipes - designed to delight your
+                senses.
+              </p>
+              <a href="#cocktails">View Cocktails</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
